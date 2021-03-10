6f1ef202786992387a78bd85bba5cde3dd5e2da9
unknown <lsu018@ucr.edu>
2021-03-10 15:30:50 -0800

    first

--- aug_sum/run.sh
+++ aug_sum/run.sh
@@ -1,15 +1,15 @@
 export CILK_NWORKERS=1
-./aug_sum 0 > aug.txt
-./aug_sum 0 >> aug.txt
+./aug_sum 10 > aug.txt
+echo ./aug_sum 10 >> aug.txt
 export CILK_NWORKERS=2
 ./aug_sum 10 >> aug.txt
-./aug_sum 10 >> aug.txt
+echo ./aug_sum 10 >> aug.txt
 export CILK_NWORKERS=4
-./aug_sum 10 >> aug.txt
-./aug_sum 10 >> aug.txt
+./aug_sum 10 >> aug.tzxt
+echo ./aug_sum 10 >> aug.txt
 export CILK_NWORKERS=8
 ./aug_sum 10 >> aug.txt
-./aug_sum 10 >> aug.txt
+echo ./aug_sum 10 >> aug.txt
 export CILK_NWORKERS=16
 ./aug_sum 10 >> aug.txt
-./aug_sum 10 >> aug.txt
+echo ./aug_sum 10 >> aug.txt
